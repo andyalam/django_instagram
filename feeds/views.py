427e424183f3c059b47bf5f9cca32d630d9cc281
andy <andyalam@hotmail.com>
2016-07-23 16:51:22 -0700

    more bootstrapping and base nav

--- feeds/views.py
+++ feeds/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def login(request):
-    return render(request, 'feeds/base.html')
+    return render(request, 'feeds/login.html')
