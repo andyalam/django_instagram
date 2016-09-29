95c262c2925ad27ae0c5a467a4564c8b6a4423ab
Andy  Alameddine <andya@bixly.com>
2016-09-29 13:43:15 -0700

    comment model, misc model updates

--- feeds/models.py
+++ feeds/models.py
@@ -5,12 +5,18 @@ from imagekit.models import ProcessedImageField
 from imagekit.processors import ResizeToFill
 
 
-#class IGUser(AbstractBaseUser, PermissionsMixin):
-
-
 class IGPost(models.Model):
+    user = models.OneToOneField(User, null=True, blank=True)
+    title = models.CharField(max_length=100)
     image = ProcessedImageField(upload_to='posts',
                                 #processors=[ResizeToFill(200,200)],
                                 format='JPEG',
                                 options={ 'quality': 100})
 
+    def __unicode__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('IGPost')
+    user = models.OneToOneField(User)
+    comment = models.CharField(max_length=100)
