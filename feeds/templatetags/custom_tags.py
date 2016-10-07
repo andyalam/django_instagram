1c31fe5897c4df70852a3006f626e0202e04751f
Andy  Alameddine <andyalam@hotmail.com>
2016-10-07 15:59:42 -0700

    custom tag to help with heart/empty heart on feed posts

---
+++ feeds/templatetags/custom_tags.py
@@ -0,0 +1,12 @@
+from django import template
+from feeds.models import Like
+
+register = template.Library()
+
+@register.simple_tag
+def has_user_liked_post(post, user):
+    try:
+        like = Like.objects.get(post=post, user=user)
+        return "fa-heart"
+    except:
+        return "fa-heart-o"
