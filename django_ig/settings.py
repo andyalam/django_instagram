5491d2e79b6bc59ec76291a49af9f0c50653bac8
ShintaG3 <snishino2013@gmail.com>
2019-06-29 23:50:06 +0900

    whitenoise config

--- django_ig/settings.py
+++ django_ig/settings.py
@@ -148,7 +148,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = '/media/'
