605ea55848bb3f4171de64142ab71cce7bb91004
ShintaG3 <snishino2013@gmail.com>
2019-06-30 19:39:34 +0900

    setting and requirements

--- django_ig/settings.py
+++ django_ig/settings.py
@@ -163,3 +163,7 @@ except ImportError as e:
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
