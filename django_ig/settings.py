e93203d4347d3bcb69fada3866230b19a5a902e7
ShintaG3 <snishino2013@gmail.com>
2019-06-30 12:39:19 +0900

    heroku database configuration

--- django_ig/settings.py
+++ django_ig/settings.py
@@ -158,3 +158,8 @@ try:
     from local_settings import *
 except ImportError as e:
     pass
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
