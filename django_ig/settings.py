d2fcdb00f668e3793dd28e4caf52778904923e60
ShintaG3 <snishino2013@gmail.com>
2019-06-30 23:11:53 +0900

    setting

--- django_ig/settings.py
+++ django_ig/settings.py
@@ -166,4 +166,6 @@ AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage' 
+AWS_BUCKET_ACL =None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
