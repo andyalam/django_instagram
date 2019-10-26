fd98df11e0a72f184fc13c24832d44afd622fc8b
alokgoldy <43291683+alokgoldy@users.noreply.github.com>
2019-10-26 12:23:57 +0530

    Update urls.py
    Added coments in urls.py file  to help begginers know where they are linked to.

--- django_ig/urls.py
+++ django_ig/urls.py
@@ -19,6 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # this is for admin page
     url(r'^admin/', admin.site.urls),
+    # this is for homepage
     url(r'^', include('feeds.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
